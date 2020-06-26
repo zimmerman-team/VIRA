38d7e28fda1575599aa02daf604283125161145d
Stefanos <stefanos@zimmermanzimmerman.nl>
2020-06-26 12:10:41 +0300

    feat: add funder model and include in report

--- server/controllers/ReportController.ts
+++ server/controllers/ReportController.ts
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Report = require('../models/report');
+const Funder = require('../models/funder');
 const Project = require('../models/project');
 const Location = require('../models/location');
 const policyPriority = require('../models/policyPriority');
@@ -32,6 +33,7 @@ export function getReports(req: any, res: any) {
     .populate('project')
     .populate('target_beneficiaries')
     .populate('policy_priority')
+    .populate('funder')
     .exec((err: any, reports: any) => {
       if (err) {
         res(JSON.stringify({ status: 'error', message: err.message }));
@@ -57,6 +59,7 @@ export function getReport(req: any, res: any) {
     .populate('project')
     .populate('target_beneficiaries')
     .populate('policy_priority')
+    .populate('funder')
     .exec((err: any, report: any) => {
       if (err) {
         res(JSON.stringify({ status: 'error', message: err.message }));
@@ -134,6 +137,29 @@ async function getPolicyPriority(data: any) {
   });
 }
 
+async function getFunder(data: any) {
+  return new Promise((resolve, reject) => {
+    Funder.findOne({ name: data }).exec((err: any, funder: any) => {
+      if (err || !funder) {
+        if (data === '') {
+          resolve(null);
+        } else {
+          Funder.create({ name: data }, (err2: any, funder2: any) => {
+            if (err2) {
+              console.log('err2', err2);
+              resolve(null);
+            } else {
+              resolve(funder2);
+            }
+          });
+        }
+      } else {
+        resolve(funder);
+      }
+    });
+  });
+}
+
 async function getLocation(data: any) {
   return new Promise((resolve, reject) => {
     if (data) {
@@ -176,39 +202,42 @@ export function addReport(req: any, res: any) {
               res(JSON.stringify({ status: 'error', message: err.message }));
             }
             getLocation(data.location).then((location: any) => {
-              // console.log(pp);
-              let report = new Report();
-              report.project = project;
-              report.title = data.title;
-              report.location = location;
-              report.place_name = data.place_name;
-              report.date = new Date().toLocaleDateString();
-              report.country = data.country;
-              report.target_beneficiaries = tb;
-              report.policy_priority = pp;
-              report.budget = data.budget;
-              report.total_target_beneficiaries =
-                data.total_target_beneficiaries;
-              report.total_target_beneficiaries_commited =
-                data.total_target_beneficiaries_commited;
-              report.budget = data.budget;
-              report.insContribution = data.insContribution;
-              report.key_outcomes = data.key_outcomes;
-              report.monitor_report_outcomes = data.monitor_report_outcomes;
-              report.media = data.media;
-              report.key_implementation_challenges =
-                data.key_implementation_challenges;
-              report.other_project_outcomes = data.other_project_outcomes;
-              report.plans = data.plans;
-              report.other_comments = data.other_comments;
-              report.isDraft = data.isDraft ? data.isDraft : false;
-              report.save((err: any, report: any) => {
-                if (err) {
-                  res(
-                    JSON.stringify({ status: 'error', message: err.message })
-                  );
-                }
-                res(JSON.stringify({ status: 'success', data: report }));
+              getFunder(data.funder).then((funder: any) => {
+                // console.log(pp);
+                let report = new Report();
+                report.project = project;
+                report.title = data.title;
+                report.location = location;
+                report.place_name = data.place_name;
+                report.date = new Date().toLocaleDateString();
+                report.country = data.country;
+                report.target_beneficiaries = tb;
+                report.policy_priority = pp;
+                report.budget = data.budget;
+                report.total_target_beneficiaries =
+                  data.total_target_beneficiaries;
+                report.total_target_beneficiaries_commited =
+                  data.total_target_beneficiaries_commited;
+                report.budget = data.budget;
+                report.insContribution = data.insContribution;
+                report.key_outcomes = data.key_outcomes;
+                report.monitor_report_outcomes = data.monitor_report_outcomes;
+                report.media = data.media;
+                report.key_implementation_challenges =
+                  data.key_implementation_challenges;
+                report.other_project_outcomes = data.other_project_outcomes;
+                report.plans = data.plans;
+                report.other_comments = data.other_comments;
+                report.isDraft = data.isDraft ? data.isDraft : false;
+                report.funder = funder;
+                report.save((err: any, report: any) => {
+                  if (err) {
+                    res(
+                      JSON.stringify({ status: 'error', message: err.message })
+                    );
+                  }
+                  res(JSON.stringify({ status: 'success', data: report }));
+                });
               });
             });
           }
@@ -274,44 +303,48 @@ export function editReport(req: any, res: any) {
                   );
                 }
                 getLocation(data.location).then((location: any) => {
-                  report.title = data.title;
-                  report.location = location;
-                  report.place_name = data.place_name;
-                  report.date = new Date().toLocaleDateString();
-                  report.country = data.country;
-                  report.target_beneficiaries = tb;
-                  report.policy_priority = pp;
-                  report.budget = data.budget;
-                  report.total_target_beneficiaries =
-                    data.total_target_beneficiaries;
-                  report.total_target_beneficiaries_commited =
-                    data.total_target_beneficiaries_commited;
-                  report.budget = data.budget;
-                  report.insContribution = data.insContribution;
-                  report.key_outcomes = data.key_outcomes;
-                  report.monitor_report_outcomes = data.monitor_report_outcomes;
-                  report.media = data.media;
-                  report.key_implementation_challenges =
-                    data.key_implementation_challenges;
-                  report.other_project_outcomes = data.other_project_outcomes;
-                  report.plans = data.plans;
-                  report.other_comments = data.other_comments;
-                  report.isDraft = data.isDraft ? data.isDraft : false;
-                  report.save((err5: any, updatedRep: any) => {
-                    if (err5) {
+                  getFunder(data.funder).then((funder: any) => {
+                    report.title = data.title;
+                    report.location = location;
+                    report.place_name = data.place_name;
+                    report.date = new Date().toLocaleDateString();
+                    report.country = data.country;
+                    report.target_beneficiaries = tb;
+                    report.policy_priority = pp;
+                    report.budget = data.budget;
+                    report.total_target_beneficiaries =
+                      data.total_target_beneficiaries;
+                    report.total_target_beneficiaries_commited =
+                      data.total_target_beneficiaries_commited;
+                    report.budget = data.budget;
+                    report.insContribution = data.insContribution;
+                    report.key_outcomes = data.key_outcomes;
+                    report.monitor_report_outcomes =
+                      data.monitor_report_outcomes;
+                    report.media = data.media;
+                    report.key_implementation_challenges =
+                      data.key_implementation_challenges;
+                    report.other_project_outcomes = data.other_project_outcomes;
+                    report.plans = data.plans;
+                    report.other_comments = data.other_comments;
+                    report.isDraft = data.isDraft ? data.isDraft : false;
+                    report.funder = funder;
+                    report.save((err5: any, updatedRep: any) => {
+                      if (err5) {
+                        res(
+                          JSON.stringify({
+                            status: 'error',
+                            message: err5.message,
+                          })
+                        );
+                      }
                       res(
                         JSON.stringify({
-                          status: 'error',
-                          message: err5.message,
+                          status: 'success',
+                          data: updatedRep,
                         })
                       );
-                    }
-                    res(
-                      JSON.stringify({
-                        status: 'success',
-                        data: updatedRep,
-                      })
-                    );
+                    });
                   });
                 });
               }
